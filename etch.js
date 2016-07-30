bc5bf4e54518036f172fb5ccc800a48aea211f83
Ron Andrews <ron.andrews@gmail.com>
2016-07-30 05:49:07 -0400

    first build

---
+++ etch.js
@@ -0,0 +1,76 @@
+var etchWidth; 
+var gradient;
+
+$(document).ready(function()
+{
+	etchWidth = parseInt($(".etch").css("width"), 10);
+	createGrid(16,16);
+});
+
+
+$('.etch').on('mouseenter', '.pixel', function() {
+/*
+	var pColor = getPixelColor($(this));
+	if (pColor - gradient > 0) {
+		pColor -= gradient;
+	} else pColor = 0;
+
+	$(this).css({"background-color": "rgb(" + pColor + ", " + pColor + "," + pColor +")"});
+	console.log($(this).css("background-color"));
+*/
+	darkenPixel($(this));
+});
+
+// Logs x, y coordinates when a pixel is clicked
+$('.etch').on('click', '.pixel', function() {
+	console.log($(this).data("col") + ", " + $(this).data("row"));
+})
+
+$('.bar').on('click', '#reset', function() {
+	var size = prompt("Enter new grid size:");
+	createGrid(size, size);
+});
+
+function createGrid(x, y) {
+	resetGrid();
+
+	// Create our grid
+	for (var row = 0; row < y; row++)
+	{
+		$(".etch").append( '<div class="row" data-row="' + row + '"></div>' );
+
+		for (var col = 0; col < x; col++)
+		{
+			$('.row[data-row = "' + row + '"]').append('<div class="pixel" data-row="' + row + '" data-col="' + col + '"></div>');
+		}
+	}
+
+	// Figure out & set the new pixel heigh & width
+	var pixelSize = etchWidth / x;
+	$(".pixel").css({"width": pixelSize, "height": pixelSize});
+	$(".row").css({"height" : pixelSize});
+
+
+	// creates steps for 10 gradients before hitting black
+	gradient = Math.round(getPixelColor($(".pixel")) / 10);
+}
+
+function darkenPixel($pixel) {
+	var pColor = getPixelColor($($pixel));
+	if (pColor - gradient > 0) {
+		pColor -= gradient;
+	} else pColor = 0;
+
+	$($pixel).css({"background-color": "rgb(" + pColor + ", " + pColor + "," + pColor +")"});
+	console.log($($pixel).css("background-color"));
+}
+
+function getPixelColor($pixel) {
+	// this will fail if the CSS doesn't use rgb()
+	var color = parseInt(String($($pixel).css("background-color")).substring(4), 10);
+	return color;
+}
+
+function resetGrid() {
+	$('.row').remove();
+}
